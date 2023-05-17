af2f6371cad828b82079906a054dbf79fd554827
Micael Gomes Targino <micaeltargino@Luizs-MacBook-Pro.local>
2023-05-16 22:27:36 -0300

    trying to solve bug

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -6,7 +6,8 @@ import { SocketContext } from '../SocketContext';
 const Notifications = () => {
     const { answerCall, call, callAccepted } = useContext(SocketContext);
     return (
-        <>
+        <>  
+        {console.log(call)}
             {call.isReceivedCall && !callAccepted && (
                 <div style={{display: 'flex', justifyContent: 'center'}}>
                     <h1>{call.name} is calling:</h1>
